52a2c239e9168218ad2bea3608469f400be6aa08
Залим Лампежев <sabolch@yandex.ru>
2016-10-22 01:17:52 +0300

    xa-header

--- includes/header.php
+++ includes/header.php
@@ -1,45 +1,44 @@
 <section class="xa-header">
-  <div class="container">
+  <div class="container xa-nopadding">
     <div class="row">
-          
-    <div class="col-md-9 col-sm-12">
-      <ul class="nav">
-        <li class=""><a href="#">О компании</a></li>
-        <li class=""><a href="#">Самовывоз / Доставка</a></li>
-        <li class=""><a href="#">Как оформить заказ</a></li>
-        <li class=""><a href="#">Фото наших работ</a></li>
-        <li class=""><a href="#">Отзывы</a></li>
-        <li class=""><a href="#">Контакты</a></li>
-      </ul>
-    </div>
+      <div class="col-md-9 col-sm-12">
+        <ul class="nav">
+          <li class=""><a href="#">О компании</a></li>
+          <li class=""><a href="#">Самовывоз / Доставка</a></li>
+          <li class=""><a href="#">Как оформить заказ</a></li>
+          <li class=""><a href="#">Фото наших работ</a></li>
+          <li class=""><a href="#">Отзывы</a></li>
+          <li class=""><a href="#">Контакты</a></li>
+        </ul>
+      </div>
 
-    <div class="col-md-3 col-sm-12 col-xs-12 xa-none">
-      <div class="xa-per-box">
-        <div class="xa-out">
-          <span class="right">Перезвонить Вам?</span>
+      <div class="col-md-3 col-sm-12 col-xs-12 xa-none">
+        <div class="xa-per-box">
+          <div class="xa-out">
+            <span class="right">Перезвонить Вам?</span>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   </div>      
        
 
-    <div class="clear"></div>
+    
 
     <div class="xa-header-center">
-      <div class="container">
+      <div class="container xa-nopadding">
         <div class="row">
           <div class="col-md-4 col-sm-12 col-xs-12">
-            <div class="col-md-2 col-sm-2 col-xs-12">
+            <div class="col-md-2 col-sm-2 col-xs-12 xa-nopadding">
               <a href="#">
                 <img class="xa-logo" src="img/logo.png" alt="">
               </a>
             </div>
-            <div class="col-md-10 col-sm-10 col-xs-12">
-              <div class="col-md-12">
+            <div class="col-md-10 col-sm-8 col-xs-12">
+              <div class="col-md-12 xa-nopadding">
                 <a href="#" class="xa-logo-a"><span class="xa-yellow">modul</span>kartina.kz</a>
               </div>
-              <div class="col-md-12">
+              <div class="col-md-12 xa-nopadding">
                 <p class="xa-im">Интернет-магазин модульных картин с доставкой по Казахстану</p>
               </div>
             </div>
@@ -67,4 +66,53 @@
         </div>
       </div>
     </div>
+
+    
+</section>
+
+<section class="xa-form-box">
+    <div class="container">
+      <div class="row">
+        <div class="xa-ybox">
+          <div class="col-md-9 col-sm-8 col-xs-6">
+            <form action="">
+              <input type="text" placeholder="Поиск по товарам">
+            </form>
+          </div>
+          <div class="col-md-3 col-sm-4 col-xs-6">
+            <div class="xa-kor">
+              <span class="xa-your">Ваша корзина</span>
+              <span class="xa-price">9 555 рублей.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class="xa-first">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2 col50 col-sm-4 col-xs-12">
+        <img src="img/xa-first1.png" alt="">
+        <p>Картины</p>
+      </div>
+      <div class="col-md-2 col50 col-sm-4 col-xs-12">
+        <img src="img/xa-first2.png" alt="">
+        <p>Модульные картины</p>
+      </div>
+      <div class="col-md-2 col50 col-sm-4 col-xs-12">
+        <img src="img/xa-first3.png" alt="">
+        <p>Постеры</p>
+      </div>
+      <div class="col-md-2 col50 col500 col-sm-6 col-xs-12">
+        <img src="img/xa-first4.png" alt="">
+        <p>Репродукции</p>
+      </div>
+      <div class="col-md-2 col50 col500 col-sm-6 col-xs-12">
+        <img src="img/xa-first5.png" alt="">
+        <p>Аксессуары</p>
+      </div>
+    </div>
+  </div>
 </section>
