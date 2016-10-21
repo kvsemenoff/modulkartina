2387f9773b5bffdc4ebd25dd57d7ab0d5e482572
engine77 <zaur.ka.00@mail.ru>
2016-10-21 10:43:17 +0300

    a

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
 	<script src="js/common.js"></script>
 
 	<?php require_once('includes/header.php'); ?>
+	<?php require_once('includes/news.php'); ?>
 	
 	<?php require_once('includes/footer.php'); ?>
 
