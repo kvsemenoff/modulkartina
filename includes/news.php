1643563234a591231f50d84be814ed0f2d2c20e4
engine77 <zaur.ka.00@mail.ru>
2016-10-21 11:09:03 +0300

    a

---
+++ includes/news.php
@@ -0,0 +1,16 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">			
+			<div class="news_text">Картины ручной работы: <br>Настоящее искусство на Ваших стенах <br><span>11.05.2016</span></div>
+			<div class="news_img"><a href="img/zk_news.png"><img src="" alt=""></a> </div>
+		</div>
+		<div class="col-md-4">			
+			<div class="news_text">В чем особенность картин modulkartina<br><span>11.05.2016</span></div>
+			<div class="news_img"><a href="img/zk_news2.png"><img src="" alt=""></a> </div>
+		</div>
+		<div class="col-md-4">			
+			<div class="news_text">Дизайнерские фотообои:<br>освежите Ваш интерьер за 30 минут!<br><span>11.05.2016</span></div>
+			<div class="news_img"><a href="img/zk_news3.png"><img src="" alt=""></a> </div>
+		</div>
+	</div>
+</div>	
