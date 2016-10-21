96e2d896722a55b57c91fe22bf46cbb313e8e991
Berzeg07 <berezg86@gmail.com>
2016-10-21 14:30:54 +0400

    17.10

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
 	<script src="js/common.js"></script>
 
 	<?php require_once('includes/header.php'); ?>
+	<?php require_once('includes/choose.php'); ?>
 	<?php require_once('includes/news.php'); ?>
 	<?php require_once('includes/slider.php'); ?>
 	<?php require_once('includes/footer.php'); ?>
