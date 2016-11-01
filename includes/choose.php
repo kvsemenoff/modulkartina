85542bca3b94bb015fcaf009d3eddd92eb389e06
Berzeg07 <berezg86@gmail.com>
2016-11-01 13:14:26 +0300

    17.198

--- includes/choose.php
+++ includes/choose.php
@@ -9,72 +9,75 @@
 	    			<span>Выбери свой вариант</span>
 	    		</div>
     		</div>
+        </div>    
             <div class="clearfix"></div>
-    	</div>
-    	<div class="ab-img-box box1">
-            <div class="ab-line ab-line2">
-                <img src="img/ab-line.png" alt="">
-            </div>
-    		<img src="img/ab-img-1.png" alt="">
-    		<div class="ab-choose-number">
-    			<span>1</span>
-    		</div>
-    		<div class="ab-choosw-txt">
-    			<span>Часть</span>
-    		</div>
-    	</div>
-    	<div class="ab-img-box box2">
-    		<img src="img/ab-img-2.png" alt="">
-    		<div class="ab-choose-number ab-choose-number2">
-    			<span>2</span>
-    		</div>
-    		<div class="ab-choosw-txt">
-    			<span>Части</span>
-    		</div>
-            <div class="ab-line">
-            <img src="img/ab-line.png" alt="">
-            </div>
-    	</div>
-    	<div class="ab-img-box box3">
-    		<img src="img/ab-img-3.png" alt="">
-    		<div class="ab-choose-number ab-choose-number3">
-    			<span>3</span>
-    		</div>
-    		<div class="ab-choosw-txt">
-    			<span>Части</span>
-    		</div>
-             <div class="ab-line">
-            <img src="img/ab-line.png" alt="">
-            </div>
-    	</div>
-    	<div class="ab-img-box box4">
-    		<img src="img/ab-img-4.png" alt="">
-    		<div class="ab-choose-number ab-choose-number3">
-    			<span>4</span>
-    		</div>
-    		<div class="ab-choosw-txt">
-    			<span>Части</span>
-    		</div>
-             <div class="ab-line">
-            <img src="img/ab-line.png" alt="">
-            </div>
-    	</div>
-    	<div class="ab-img-box box5">
-            <div class="ab-line">
+        	<div class="ab-img-box box1">
+                <div class="ab-line ab-line2">
                     <img src="img/ab-line.png" alt="">
                 </div>
-        		<img src="img/ab-img-5.png" alt="">
-        		<div class="ab-choose-number ab-choose-number3">
-        			<span>5</span>
+        		<img src="img/ab-img-1.png" alt="">
+        		<div class="ab-choose-number">
+        			<span>1</span>
         		</div>
         		<div class="ab-choosw-txt">
-        			<span>Частей</span>
+        			<span>Часть</span>
         		</div>
-                 <div class="ab-line ab-line2">
+        	</div>
+        	<div class="ab-img-box box2">
+        		<img src="img/ab-img-2.png" alt="">
+        		<div class="ab-choose-number ab-choose-number2">
+        			<span>2</span>
+        		</div>
+        		<div class="ab-choosw-txt">
+        			<span>Части</span>
+        		</div>
+                <div class="ab-line">
                 <img src="img/ab-line.png" alt="">
                 </div>
         	</div>
-    	<div class="clearfix"></div>
-    </div>
+        	<div class="ab-img-box box3">
+        		<img src="img/ab-img-3.png" alt="">
+        		<div class="ab-choose-number ab-choose-number3">
+        			<span>3</span>
+        		</div>
+        		<div class="ab-choosw-txt">
+        			<span>Части</span>
+        		</div>
+                 <div class="ab-line">
+                <img src="img/ab-line.png" alt="">
+                </div>
+        	</div>
+        	<div class="ab-img-box box4">
+        		<img src="img/ab-img-4.png" alt="">
+        		<div class="ab-choose-number ab-choose-number3">
+        			<span>4</span>
+        		</div>
+        		<div class="ab-choosw-txt">
+        			<span>Части</span>
+        		</div>
+                 <div class="ab-line">
+                <img src="img/ab-line.png" alt="">
+                </div>
+        	</div>
+        	<div class="ab-img-box box5">
+                <div class="ab-line">
+                        <img src="img/ab-line.png" alt="">
+                    </div>
+            		<img src="img/ab-img-5.png" alt="">
+            		<div class="ab-choose-number ab-choose-number3">
+            			<span>5</span>
+            		</div>
+            		<div class="ab-choosw-txt">
+            			<span>Частей</span>
+            		</div>
+                     <div class="ab-line ab-line2">
+                    <img src="img/ab-line.png" alt="">
+                    </div>
+            	</div>
+            </div>    
+        	<div class="clearfix"></div>
+            </div>
+        </div>    
 
+    </div>
 </section>
